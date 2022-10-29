48e7df6985f9da02eaf202eb9610ce2bff21dec4
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2022-10-29 22:02:18 +0300

    Поправил решение

--- Firsov Ruslan_11.10_5.py
+++ Firsov Ruslan_11.10_5.py
@@ -12,3 +12,24 @@ salary, expenses = 10000, 12000
 sum_expenses = 12000 * 1.03 * 11
 sum_salary = 10000 * 12
 print(f'Необходимо взять в долг {sum_expenses - sum_salary} рублей')
+
+# Траты растут ежемесячно по три процента от предыдущей суммы месяца.
+# Поэтому реализация выглядит так
+
+salary, expenses = 10000, 12000
+i = 0
+months_expenses = 0
+money_needs = 0
+
+while i < 10:
+
+    if i == 0:
+        months_expenses = expenses
+    elif i >= 1:
+        months_expenses *= 1.03
+    i += 1
+    difference = round(months_expenses-salary, 2)
+    money_needs += difference
+    print('Расходы в', i, 'месяце:', difference, ', Итого за', i, 'мес.:', round(money_needs,2))
+
+print('Сотруднику необходимо взять', money_needs, 'рублей')
